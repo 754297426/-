1d48491a026d2479facb24376b7fd7cb2b2c0a62
754297426 <50744996+754297426@users.noreply.github.com>
2020-09-14 15:49:52 +0800

    Update 201910623022-1-6.c

--- 201910623022-1-6.c
+++ 201910623022-1-6.c
@@ -2,8 +2,8 @@
 int main()
 {
 	printf("******************************************\n");
-	printf("*                name:Áõ¼Î½ú             *\n");
-	printf("*                class£º¹úº½191          *\n");
+	printf("*                name:Anduin             *\n");
+	printf("*                class:191               *\n");
 	printf("*                No.:201910623022        *\n");
 	printf("******************************************\n");
 	return 0;
